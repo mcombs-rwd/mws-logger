413cffcfcdd4199853fffcb965bafc2aa3fceee6
Mike Combs <mcombs@riggsward.com>
2024-04-07 20:57:52 -0400

    feat: create tables

--- data_server/water/database.py
+++ data_server/water/database.py
@@ -1,8 +1,13 @@
 import click
 from datetime import datetime, timedelta
 from flask import current_app, g
+from pathlib import Path
 from random import randrange
 import sqlite3
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+
+from models import (Base, Water_reading, Sensor)
 
 """Create and populate db
 
@@ -12,34 +17,39 @@ Enter `python -m flask --app water <command>`.
 
 def init_app(app):
     app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
-    app.cli.add_command(add_test_command)
+    app.cli.add_command(init_db)
+    # app.cli.add_command(add_test_data)
 
-@click.command("init-db")
-def init_db_command():
-    db = get_db()
-    with current_app.open_resource("./sql-statements/schema.sql") as f:
-        db.executescript(f.read().decode("utf-8"))
+# @click.command("init-db")
+def init_db():
+    db_filepath = Path("water.sqlite")
+    if db_filepath.exists():
+        db_filepath.unlink()
+
+    engine = sa.create_engine("sqlite:///" + str(db_filepath), echo=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
     click.echo("You have initialized the database.")
     click.echo("You can load test data with `add-test-data`.")
 
-@click.command("add-test-data")
-def add_test_command():
-    start_date = datetime.fromisoformat("2024-03-01 00:00:00") # yyyy-mm-dd hh:mm:ss
-    db = get_db()
-    for sensor in ['green_roof', 'rain_gauge']:
-        meter_value = 0
-        for days in range(61):
-            for hours in range(24):
-                logged_date = start_date + timedelta(days=days, hours=hours)
-                gauge_value = randrange(0, 50, 1)
-                meter_value += gauge_value
-                db.execute(
-                    "INSERT INTO water (logged_date, sensor, gauge_value, meter_value) VALUES (?, ?, ?, ?)",
-                    (logged_date.isoformat(sep=" "), sensor, gauge_value, meter_value)
-                )
-    db.commit()
-    click.echo("You have loaded test data into the database!")
+# # @click.command("add-test-data")
+# def add_test_data():
+#     start_date = datetime.fromisoformat("2024-03-01 00:00:00") # yyyy-mm-dd hh:mm:ss
+#     water_meter = Sensor(id="green_roof", name="Green Roof", )
+#     db = get_db()
+#     for sensor in ['green_roof', 'rain_gauge']:
+#         meter_value = 0
+#         for days in range(61):
+#             for hours in range(24):
+#                 logged_date = start_date + timedelta(days=days, hours=hours)
+#                 gauge_value = randrange(0, 50, 1)
+#                 meter_value += gauge_value
+#                 db.execute(
+#                     "INSERT INTO water (logged_date, sensor, gauge_value, meter_value) VALUES (?, ?, ?, ?)",
+#                     (logged_date.isoformat(sep=" "), sensor, gauge_value, meter_value)
+#                 )
+#     db.commit()
+#     click.echo("You have loaded test data into the database!")
 
 def get_db():
     if "db" not in g:
@@ -54,3 +64,6 @@ def close_db(e=None):
     db = g.pop("db", None)
     if db is not None:
         db.close()
+
+if __name__ == "__main__":
+    init_db_command()
---
+++ data_server/water/models.py
@@ -0,0 +1,34 @@
+from typing import Optional
+from sqlalchemy.orm import (Mapped, mapped_column, declarative_base)
+
+from datetime import datetime
+
+Base = declarative_base()
+
+
+class Water_reading(Base):
+    """Recorded measurements from water meters and rain gauges"""
+    __tablename__ = "water_reading"
+    id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    date: Mapped[datetime]
+    sensor_id: Mapped[str]
+    value: Mapped[int]
+
+    def __repr__(self) -> str:
+        return f"<Water_reading(id={self.id}, date={self.date}, sensor={self.sensor}, value={self.value})>"
+
+
+class Sensor(Base):
+    """Water flowmeters and rain gauges
+
+    Water flow meters report total gallons, they are cumulative.
+    Rain gauges report current inches
+    """
+    __tablename__ = "sensor"
+    id: Mapped[str] = mapped_column(primary_key=True, unique=True)
+    name: Mapped[Optional[str]]
+    cumulative: Mapped[bool]
+    units: Mapped[str]
+
+    def __repr__(self) -> str:
+        return f"<Sensor(id={self.id}, name={self.name}, cumulative={self.cumulative})>"
