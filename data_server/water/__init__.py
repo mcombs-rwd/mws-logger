6bedcb809134ed2227bdfa0e38d584fce9dc9880
Mike Combs <mcombs@riggsward.com>
2024-04-07 21:21:10 -0400

    feat: add test data

--- data_server/water/__init__.py
+++ data_server/water/__init__.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
 from flask import Flask
 import os
+import sqlalchemy
 import sqlite3
 
-from water import pages, database, errors
+import database, errors, pages
 
 load_dotenv()
 
@@ -13,6 +14,7 @@ def create_app():
     # app.logger.setLevel("INFO")
     app.logger.debug(f"Using database: {os.getenv('FLASK_DATABASE')}")
     app.logger.debug(f"{sqlite3.sqlite_version=}")
+    app.logger.debug(f"{sqlalchemy.__version__=}")
     app.register_blueprint(pages.bp)
     app.register_error_handler(404, errors.page_not_found)
     database.init_app(app)
--- data_server/water/database.py
+++ data_server/water/database.py
@@ -9,6 +9,9 @@ from sqlalchemy.orm import sessionmaker
 
 from models import (Base, Water_reading, Sensor)
 
+db_filepath = Path("water.db")
+db_url = "sqlite:///" + str(db_filepath)
+
 """Create and populate db
 
 Enter `python -m flask --app water <command>`.
@@ -22,34 +25,39 @@ def init_app(app):
 
 # @click.command("init-db")
 def init_db():
-    db_filepath = Path("water.sqlite")
     if db_filepath.exists():
         db_filepath.unlink()
 
-    engine = sa.create_engine("sqlite:///" + str(db_filepath), echo=True)
+    engine = sa.create_engine(db_url, echo=True)
     Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
     click.echo("You have initialized the database.")
     click.echo("You can load test data with `add-test-data`.")
 
-# # @click.command("add-test-data")
-# def add_test_data():
-#     start_date = datetime.fromisoformat("2024-03-01 00:00:00") # yyyy-mm-dd hh:mm:ss
-#     water_meter = Sensor(id="green_roof", name="Green Roof", )
-#     db = get_db()
-#     for sensor in ['green_roof', 'rain_gauge']:
-#         meter_value = 0
-#         for days in range(61):
-#             for hours in range(24):
-#                 logged_date = start_date + timedelta(days=days, hours=hours)
-#                 gauge_value = randrange(0, 50, 1)
-#                 meter_value += gauge_value
-#                 db.execute(
-#                     "INSERT INTO water (logged_date, sensor, gauge_value, meter_value) VALUES (?, ?, ?, ?)",
-#                     (logged_date.isoformat(sep=" "), sensor, gauge_value, meter_value)
-#                 )
-#     db.commit()
-#     click.echo("You have loaded test data into the database!")
+# @click.command("add-test-data")
+def add_test_data():
+    engine = sa.create_engine(db_url, echo=True)
+    Session = sessionmaker(bind=engine)
+    start_date = datetime.fromisoformat("2024-03-01 00:00:00") # yyyy-mm-dd hh:mm:ss
+    water_meter = Sensor(id="green_roof", name="Green Roof", cumulative=True, units="gallons")
+    rain_gauge = Sensor(id="rain_gauge", name="NASH Rain Gauge", cumulative=False, units="inches")
+    sensors = [water_meter, rain_gauge]
+    with Session() as session:
+        for sensor in sensors:
+            session.add(sensor)
+            meter_value = 0
+            for days in range(61):
+                for hours in range(24):
+                    date = start_date + timedelta(days=days, hours=hours)
+                    gauge_value = randrange(0, 50, 1)
+                    meter_value += gauge_value
+                    reading = Water_reading(
+                        date=date, sensor_id=sensor.id, 
+                        value= meter_value if sensor.cumulative else gauge_value
+                    )
+                    session.add(reading)
+        session.commit()
+    click.echo("You have loaded test data into the database!")
 
 def get_db():
     if "db" not in g:
@@ -66,4 +74,5 @@ def close_db(e=None):
         db.close()
 
 if __name__ == "__main__":
-    init_db_command()
+    init_db()
+    add_test_data()
